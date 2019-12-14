198ef8e5f6ac84f992b8eb12086bacf01b185cd9
VooDooS <thevoodoos@gmail.com>
2019-12-14 13:03:15 +0100

    Add overflow x hidden

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,10 @@
 <!doctype html>
 <html {!! get_language_attributes() !!}>
   @include('partials.head')
-  <body @php body_class(['bg-dotted','bg-fixed']) @endphp>
+  <body @php body_class([
+    'bg-dotted',
+    'bg-fixed',
+    'overflow-x-hidden']) @endphp>
     @php do_action('get_header') @endphp
     @include('partials.header')
     @yield('before')
