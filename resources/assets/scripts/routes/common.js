9f5e1c36a3300a79396fa256c7443d4e2e35b6b5
VooDooS <thevoodoos@gmail.com>
2019-12-24 08:24:35 +0100

    First sketch of autocomplete

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -1,7 +1,45 @@
+import 'corejs-typeahead/dist/typeahead.bundle.min'
+
 export default {
   init() {
     // JavaScript to be fired on all pages
-    $('#menu-switch').click(() => $('#menu').toggleClass('open'))
+
+    /* Menu button */
+    $('#menu-switch').click(() => $('#menu').toggleClass('open'));
+    
+    /* Typeahead */
+
+    var substringMatcher = function(strs) {
+      return function findMatches(q, cb) {
+        var matches, substrRegex;
+
+        // an array that will be populated with substring matches
+        matches = [];
+
+        // regex used to determine if a string contains the substring `q`
+        substrRegex = new RegExp(q, 'i');
+
+        // iterate through the pool of strings and for any string that
+        // contains the substring `q`, add it to the `matches` array
+        $.each(strs, function(i, str) {
+          if (substrRegex.test(str)) {
+            matches.push(str);
+          }
+        });
+
+        cb(matches);
+      };
+    };
+
+    $('.main-search').typeahead({
+      hint: true,
+      highlight: true,
+      minLength: 1
+    },
+    {
+      name: 'states',
+      source: substringMatcher(terms_res_types)
+    });
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,9 +22,25 @@
     </div>
     @php do_action('get_footer') @endphp
     @include('partials.footer')
-    @php wp_footer() @endphp
+
     <div class="h-40 w-full overflow-hidden bg-accent"
     style="background: url(@asset('images/temp/footer.jpg')); background-size:cover; background-position: center">
     </div>
+
+    {{-- Populate auto-complete with categories, tags, etc --}}
+    <script type="text/javascript">
+      var terms_res_types = [
+        @php
+        $categories = get_terms('res_types', array(
+            'hide_empty' => false,
+        ) );
+        foreach($categories as $category) {
+          echo "'" .$category->name . "'," ;
+        }
+        @endphp
+      ];
+    </script> 
+
+    @php wp_footer() @endphp
   </body>
 </html>
