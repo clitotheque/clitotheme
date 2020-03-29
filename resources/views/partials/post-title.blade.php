0aa26e60b49e7a7cf827548ff66527b38e18b69c
Ulysse Gérard <thevoodoos@gmail.com>
2020-03-29 15:03:46 +0200

    Tweak post titles

--- resources/views/partials/post-title.blade.php
+++ resources/views/partials/post-title.blade.php
@@ -1,7 +1,7 @@
 <header class="post-title mb-8">
-    <h2>{!! $r->get_creators_html() !!}</h2>
-    <h1 class="entry-title title-{{ $r->type_label }} -mt-6">{!! get_the_title() !!}</h1>
-    <h6 class="-mt-4 italic">
+    <h2 class="leading-none">{!! $r->get_creators_html() !!}</h2>
+    <h1 class="entry-title title-{{ $r->type_label }} leading-none">{!! get_the_title() !!}</h1>
+    <h6 class="italic leading-none">
         {!! $r->get_categories_html() !!}
     </h6>
 </header>
