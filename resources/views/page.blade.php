fd81dacf60492b838d36d21d229ca487b8eb3329
Ulysse Gérard <thevoodoos@gmail.com>
2020-04-19 13:43:23 +0200

    Basic pages

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.page')
 
-@section('main')
+@section('content')
   @while(have_posts()) @php the_post() @endphp
     @include('partials.page-header')
     @include('partials.content-page')
--- resources/views/partials/page-header.blade.php
+++ resources/views/partials/page-header.blade.php
@@ -1,6 +1,5 @@
 <div class="page-header">
   <h1 class="text-center uppercase mt-10">
-    Nouveaux contenus
-    {{-- TODO !! App::title() !! --}}
+    {!! App::title() !!}
   </h1>
 </div>
