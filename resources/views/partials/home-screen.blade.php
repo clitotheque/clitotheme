b04c78aa160a38b88677ac44c2105724dd34378e
Ulysse Gérard <thevoodoos@gmail.com>
2020-05-08 15:12:35 +0200

    Link button

--- resources/views/partials/home-screen.blade.php
+++ resources/views/partials/home-screen.blade.php
@@ -17,7 +17,7 @@
         </form>
         </div> --}}
         <div class="flex flex-row flex-wrap justify-around z-40">
-          <a href="#"><input type="button" value="Proposer un contenu" class="btn-home flex-grow m-2 p-6" /></a>
+          <a href="{{ App\Tools::poly_get_page_link("proposer-une-ressource") }}"><input type="button" value="Proposer un contenu" class="btn-home flex-grow m-2 p-6" /></a>
         <a href="{{ App\Tools::poly_get_page_link("search") }}">
           <input type="button" value="Découvrir les contenus" class="btn-home flex-grow m-2 p-6" /></a>
         </div>
