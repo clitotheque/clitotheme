970bbed69b01d03789c6935b8c5180cf137991e5
vds <vds>
2020-09-06 20:06:05 +0200

    Slider params debug

--- resources/assets/scripts/routes/home.js
+++ resources/assets/scripts/routes/home.js
@@ -8,12 +8,12 @@ export default {
   finalize() {
     // JavaScript to be fired on the home page, after the init JS
     $("#featured_slider").slick({
-      "infinite": true,
-      "arrows": false,
-      "autoplay": true,
-      "autoplaySpeed": 3000,
-      "slidesToShow": 3,
-      "slidesToScroll": 1,
+      infinite: true,
+      arrows: false,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      slidesToShow: 3,
+      slidesToScroll: 1,
       responsive: [{
         breakpoint: 1100,
         settings: {
