678737d99a33350fea2cefa0453ab21ad155cdfa
Ulysse Gérard <thevoodoos@gmail.com>
2020-09-09 20:32:40 +0200

    Debug slider

--- resources/assets/scripts/routes/home.js
+++ resources/assets/scripts/routes/home.js
@@ -11,29 +11,30 @@ export default {
       infinite: true,
       arrows: false,
       autoplay: true,
+      mobileFirst: true,
       autoplaySpeed: 3000,
-      slidesToShow: 3,
+      slidesToShow: 1,
       slidesToScroll: 1,
       swipeToSlide: true,
       responsive: [{
         breakpoint: 2000,
         settings: {
-          slidesToShow: 4
+          slidesToShow: 5
         }
       }, {
         breakpoint: 1500,
         settings: {
-          slidesToShow: 3
+          slidesToShow: 4
         }
       }, {
         breakpoint: 1050,
         settings: {
-          slidesToShow: 2
+          slidesToShow: 3
         }
       }, {
         breakpoint: 630,
         settings: {
-          slidesToShow: 1
+          slidesToShow: 2
         }
       }]
     });
