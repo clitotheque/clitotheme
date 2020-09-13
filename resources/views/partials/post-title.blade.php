98dfc70e99cbf921443813f2aede4b2560eacca1
Ulysse Gérard <thevoodoos@gmail.com>
2020-09-13 13:56:37 +0200

    Small like releated tweak

--- resources/views/partials/post-title.blade.php
+++ resources/views/partials/post-title.blade.php
@@ -3,7 +3,7 @@ if (!isset($post_link)) $post_link = false;
 if (!isset($more)) $more = false;
 //var_dump($r);
 @endphp
-<header class="post-title mb-10">
+<header class="post-title @if (!$more) mb-10 @endif">
     <h1 class="entry-title title-{{ $r->type_label }} leading-none">
         @if ($post_link)
         <a
