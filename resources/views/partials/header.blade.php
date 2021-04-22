c99ddbd77bb97ce7057e5bbe60dbe1c7325157e4
Ulysse Gérard <thevoodoos@gmail.com>
2021-04-22 18:35:12 +0200

    Add twitter icon

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -25,6 +25,8 @@
       <img width="25px" class="inline-block top-right-icon" src="@asset('images/icons/facebook.svg')" /></a>
       <a href="https://www.instagram.com/clitotheque" target="_blank" alt="Instagram">
       <img width="25px" class="inline-block top-right-icon" src="@asset('images/icons/insta.svg')" /></a>
+      <a href="https://twitter.com/ClitoTheque" target="_blank">
+      <img class="hidden w-5 lg:w-6 md:inline-block top-right-icon" src="@asset('images/icons/twitter.png')" alt="Twitter" /></a>
       <a href="mailto:contact@clitotheque.org" target="_blank" alt="Email">
       <img width="25px" class="inline-block top-right-icon" src="@asset('images/icons/emailr.svg')" /></a>
     </div>
@@ -38,6 +40,8 @@
 <img  class="hidden w-5 lg:w-6 md:inline-block top-right-icon" src="@asset('images/icons/facebook.svg')" alt="Facebook" /></a>
 <a href="https://www.instagram.com/clitotheque" target="_blank">
 <img class="hidden w-5 lg:w-6 md:inline-block top-right-icon" src="@asset('images/icons/insta.svg')" alt="Instagram" /></a>
+<a href="https://twitter.com/ClitoTheque" target="_blank">
+<img class="hidden w-5 lg:w-6 md:inline-block top-right-icon" src="@asset('images/icons/twitter.png')" alt="Twitter" /></a>
 <a href="mailto:contact@clitotheque.org" target="_blank">
 <img class="hidden w-5 lg:w-6 md:inline-block top-right-icon-mail mr-1" src="@asset('images/icons/emailr.svg')" alt="Email" /></a>
 </div>
